38ded05a02dcc6e6a72687735e456d78d53ac0f5
filipmarcinkowski <filipmarcinkowski26@gmail.com>
2024-02-05 22:34:59 +0100

    cscs

--- script.js
+++ script.js
@@ -16,84 +16,6 @@ const emptyEmailMsg = `${warningIcon} <p class="input__error-msg warning-email" 
 
 // ////////////////////////////////////////////////////////////////////////
 
-// const confirmData = btn.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   // SPRAWDZANIE CZY POLA SĄ WYPEŁNIONE////////////
-//   inputBox.forEach(function (input) {
-//     const inputField = input.querySelector('.cta-input');
-//     const warning = input.querySelectorAll('.warning');
-//     const warningEmail = input.querySelectorAll('.warning-email');
-//     const errorClr = inputField.classList.contains('error');
-//     const errorEmail = inputField.classList.contains('error-email');
-//     const emailField = inputField.classList.contains('form__email');
-
-//     // email field
-
-//     const checkAdress = function (el) {
-//       const email = inputEmail.value;
-//       const a = email.slice(0, email.indexOf('@')).length;
-//       const b = email.slice(email.indexOf('@'), email.indexOf('.')).length;
-//       const c = email.slice(email.indexOf('.'), email.length).length;
-
-//       const correctEmail = a >= 1 && b >= 3 && c >= 3;
-
-//       console.log(`a=${a}, b=${b}, c=${c}`);
-
-//       if (
-//         // jeśli email nie zawiera '@' ani '.' i
-//         (!email.includes('@', '.') && !errorEmail) ||
-//         (!correctEmail && !errorEmail)
-//       ) {
-//         inputEmail.insertAdjacentHTML('afterend', emptyEmailMsg);
-//         inputField.classList.remove('error');
-//         inputField.classList.add('error-email');
-
-//         // usuń error msg i error icon
-//         warning.forEach(function (err) {
-//           err.classList.add('hide');
-//         });
-//       } else if (correctEmail === true) {
-//         warning.forEach(function (err) {
-//           // usuń error msg i error icon
-//           err.classList.add('hide');
-//           // usuń czerwona obwódke
-//           inputField.classList.remove('error-email');
-//         });
-//         warningEmail.forEach(function (err) {
-//           // usuń error msg i error icon
-//           err.classList.add('hide');
-//           // usuń czerwona obwódke
-//           inputField.classList.remove('error-email');
-//         });
-//       }
-//     };
-
-//     // other fields
-
-//     if (inputField.value === '' && !errorClr) {
-//       // dodaj HTML z ostrzeżeniem i czerwona obwódke
-//       inputField.insertAdjacentHTML('afterend', emptyInputMsg);
-//       inputField.classList.add('error');
-//       warningEmail.forEach(function (err) {
-//         // usuń error msg i error icon
-//         err.classList.add('hide');
-//       });
-
-//       // /////jesli input nie jest pusty ///////////////
-//     } else if (inputField.value !== '' && emailField) {
-//       checkAdress();
-//     } else if (inputField.value !== '') {
-//       // usuń czerwona obwódke
-//       inputField.classList.remove('error');
-
-//       // usuń error msg i error icon
-//       warning.forEach(function (err) {
-//         err.classList.add('hide');
-//       });
-//     }
-//   });
-// });
-
 // /////////////////////////////////////////////////////////////////////////////////
 const btn = document.querySelector('.btn');
 const inputEmail = document.querySelector('.form__email');
@@ -120,14 +42,12 @@ const checkInput = function (field) {
     if (e !== inputField) {
       e.remove();
       inputField.classList.remove('error');
-      // czy to to????????????
       inputField.classList.remove('error-email');
     }
   };
   // jeśli pole jest puste //////////////////////
 
   if (isEmpty === '' && !msgClr) {
-    // isEmail === '' && !emailClr;
     addError(field);
 
     // pole nie jest puste
@@ -135,8 +55,8 @@ const checkInput = function (field) {
     [...inputEmail.parentElement.children].forEach(function (e) {
       if (e !== inputEmail) {
         e.remove();
-        inputEmail.classList.remove('error');
         inputEmail.classList.remove('error-email');
+        inputEmail.classList.remove('error');
       }
     });
     addError(field);
